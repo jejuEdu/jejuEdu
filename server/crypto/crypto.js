8e5787599713f1ce5cb72e1f1f234b906a87315d
DHAPARK <pdh4182@gmail.com>
2023-02-01 23:06:36 +0900

    fix: 사용자의 설문결과 리스트 예외 처리 구분 / 휴대폰 번호 db 와 비교하는 로직 개선 및 swagger 업데이트

--- server/crypto/crypto.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-module.exports = {
-    algorithm: 'aes-256-cbc',
-    // salt: 'encodingsalt',
-    key: 'encodingKey',
-    iv: crypto.randomBytes(32), // 초기화 벡터 랜덤 생성
-}
